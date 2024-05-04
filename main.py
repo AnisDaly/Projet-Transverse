d8cbebe3e5e0b79ff2e883b53a634517cde5be51
Zestellar <gossantoine@gmail.com>
2024-05-04 13:25:31 +0200

    mvt balle finie, commencement reset jeu après panier

--- main.py
+++ main.py
@@ -30,60 +30,86 @@ class Ball(pygame.sprite.Sprite):
         self.has_been_moved = False  # Pour vérifier si la balle a déjà été déplacée
         self.pressed=False
         self.L_points=[]
-        self.current_point_index=0
+        self.current_point_index=28
+        self.points_non_utilises=0
         self.L_aff=[]
+        self.Panier=False
+        self.Compteur=0
 
     def update(self, event):
         # Gérer les événements de la souris pour déplacer la balle une seule fois
         mouse_pos=pygame.mouse.get_pos()
-        if not self.has_been_moved:
+        #print(not event and not self.pressed)
+        if event and not self.has_been_moved:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if self.rect.collidepoint(event.pos):
+                    print("1")
                     self.is_dragging = True  # Commence à glisser si on clique sur la balle
             elif event.type == pygame.MOUSEBUTTONUP:
                 if self.is_dragging:
                     self.is_dragging = False  # Arrête de glisser quand on relâche le bouton
                     self.has_been_moved = True  # Marque la balle comme déplacée
-                    self.x, self.y = pygame.mouse.get_pos()
+                    self.x, self.y = mouse_pos
+                    distance=math.sqrt((850 - self.x) ** 2 + (398 - self.y)**2)
+                    print(distance)
+                    if distance < 50:
+                        self.points_non_utilises=19
+                    elif distance < 100:
+                        self.points_non_utilises=17
+                    elif distance < 250:
+                        self.points_non_utilises=15
+                    elif distance < 500:
+                        self.points_non_utilises=13
             elif event.type == pygame.MOUSEMOTION and self.is_dragging:
                 self.rect.move_ip(event.rel)  # Déplace la balle avec le mouvement de la souris
+        # Déplacer la balle vers le point actuel
+        elif self.L_points != [] and not event and not self.pressed and not self.Panier and self.has_been_moved:
+            if self.current_point_index>=self.points_non_utilises:
+                target_x, target_y = self.L_points[self.current_point_index]
+                dx = target_x - self.x
+                dy = target_y - self.y
+                distance = math.sqrt(dx ** 2 + dy ** 2)
+                speed = 7  # Vitesse de déplacement de la balle
+                if distance > speed:
+                    self.x += dx * speed / distance
+                    self.y += dy * speed / distance
+                else:
+                    # Si la balle est suffisamment proche du point, passer au point suivant
+                    self.current_point_index -= 1
+                # Mettre à jour la position de la balle
+                self.rect.center = (self.x, self.y)
+                if math.sqrt((850 - self.x) ** 2 + (398 - self.y)**2)<60 and not self.Panier:
+                    print("PANIER")
+                    self.Panier=True
+        elif not event and self.Panier:
+            self.Panier=False
+            self.Compteur+=1
+            print(self.Compteur)
+            self.x,self.y = WIDTH//2, HEIGHT//2
+            self.has_been_moved=False
+            self.rect.center = (self.x, self.y)
         else:
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event and event.type == pygame.MOUSEBUTTONDOWN:
                 self.pressed=True
-            elif event.type == pygame.MOUSEBUTTONUP:
+            elif event and event.type == pygame.MOUSEBUTTONUP:
                 self.pressed=False
                 self.L_aff.clear()
-            elif self.pressed and event.type == pygame.MOUSEMOTION:
-                g=(pygame.mouse.get_pos()[1]-self.y)/100
-                hypothenus=math.sqrt((pygame.mouse.get_pos()[1]-self.y)**2+(pygame.mouse.get_pos()[0]-self.x)**2)
-                if self.x > pygame.mouse.get_pos()[0] or self.x < pygame.mouse.get_pos()[0]:
-                    cos_a=(pygame.mouse.get_pos()[0]-self.x)/hypothenus
+            elif event and self.pressed and event.type == pygame.MOUSEMOTION:
+                g = (pygame.mouse.get_pos()[1]-self.y)
+                hypothenuse = math.sqrt((mouse_pos[1]-self.y)**2+(mouse_pos[0]-self.x)**2)
+                if self.x > mouse_pos[0] or self.x < mouse_pos[0]:
+                    cos_a=(mouse_pos[0]-self.x)/hypothenuse
                     self.L_aff.clear()
-                    for x_p in range(0,300,25):
-                        y=(-g*x_p**2)/(2*cos_a**2*hypothenus**2)+((mouse_pos[1]-self.y)/(mouse_pos[0]-self.x))*x_p+mouse_pos[1]-self.y
-                        self.L_aff.append((self.x+x_p,self.y+y))
-                    self.L_points=self.L_aff
-        # Déplacer la balle vers le point actuel
-        if self.L_points!=[] and not self.pressed:
-            target_x, target_y = self.L_points[self.current_point_index]
-            dx = target_x - self.x
-            dy = target_y - self.y
-            distance = math.sqrt(dx ** 2 + dy ** 2)
-            speed = 5  # Vitesse de déplacement de la balle
-            if distance > speed:
-                self.x += dx * speed / distance
-                self.y += dy * speed / distance
-            else:
-                # Si la balle est suffisamment proche du point, passer au point suivant
-                self.current_point_index = (self.current_point_index + 1) % len(self.L_points)
+                    for x_p in range(-700,25,25):
+                        y=(-g*x_p**2)/(2*cos_a**2*hypothenuse**2)+((mouse_pos[1]-self.y)/(mouse_pos[0]-self.x))*x_p*2+mouse_pos[1]-self.y
+                        self.L_aff.append((self.x-x_p,self.y-y))
+                    self.L_points=list(self.L_aff)
 
-        # Mettre à jour la position de la balle
-        self.rect.center = (self.x, self.y)
 
 
 
 # Création de la balle
-ball = Ball(250, 250)
+ball = Ball(WIDTH//2,HEIGHT//2)
 
 # Groupe de sprites contenant la balle
 all_sprites = pygame.sprite.Group()
@@ -95,19 +121,19 @@ clock = pygame.time.Clock()
 while running:
     # Dessiner l'arrière-plan
     SCREEN.blit(background_image, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        else:
-            ball.update(event)  # Met à jour la position de la balle en fonction des événements de la souris
-
-
+    event=pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = False
+    elif event.type == pygame.NOEVENT:
+        ball.update(False)
+    else:
+        ball.update(event)  # Met à jour la position de la balle en fonction des événements de la souris
     # Dessiner la balle
     all_sprites.draw(SCREEN)
 
-    for pos in ball.L_aff:
-        pygame.draw.circle(SCREEN, (255, 255, 255), pos, 5)
-
+    for i in range(len(ball.L_aff)-1,ball.points_non_utilises+5,-1):
+        pygame.draw.circle(SCREEN, (255, 255, 255), ball.L_aff[i], 5)
+    pygame.draw.rect(SCREEN, (255,255,255),(850,381,100,10), 5 )
     # Mise à jour de l'affichage
     pygame.display.flip()
 
